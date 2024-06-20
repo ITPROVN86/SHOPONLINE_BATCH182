9a7332aeb775ff853c6ef003def7786254346bc9
ITPROVN86 <quangltn3@fe.edu.vn>
2024-06-20 18:26:52 +0700

    Đẩy view Index

--- ShopBusiness/Models/Product.cs
+++ ShopBusiness/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShopBusiness.Models;
 
@@ -7,25 +8,30 @@ public partial class Product
 {
     public int ProductId { get; set; }
 
+    [Display(Name ="Tên Sản phẩm")]
     public string? ProductName { get; set; }
 
+    [Display(Name = "Mô tả")]
     public string? Description { get; set; }
-
+    [Display(Name = "Nội dung")]
     public string? Ncontent { get; set; }
-
     public int CategoryId { get; set; }
-
+    [Display(Name = "Ảnh")]
     public string? ImageUrl { get; set; }
-
+    [Display(Name = "GIá cả")]
     public decimal Price { get; set; }
 
+    [Display(Name = "Ngày đăng")]
     public DateTime CreatePost { get; set; }
 
     public int UserPost { get; set; }
 
+    [Display(Name = "Trạng thái")]
     public bool Status { get; set; }
 
+    [Display(Name = "Danh mục Sản phẩm")]
     public virtual Category Category { get; set; } = null!;
 
+    [Display(Name = "Người đăng")]
     public virtual User UserPostNavigation { get; set; } = null!;
 }
